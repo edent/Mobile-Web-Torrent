3517a63bc7e859932a6d019c37b92d26bd2786c0
Terence Eden <github.com@shkspr.mobi>
2011-11-25 10:35:13 +0000

    Changed some wording

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>eXtreme Mobile Torrent Search</title> 
+		<title>Mobile Torrent Search</title> 
 		<meta name="viewport" content="width=device-width, initial-scale=1"> 
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
